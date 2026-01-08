644761adad7a44a80735f87469f58152f0525e39
up1 <somkiat.p@gmail.com>
2026-01-08 13:07:50 +0700

    Add controller

--- demo/src/main/java/com/example/demo/users/UserController.java
+++ demo/src/main/java/com/example/demo/users/UserController.java
@@ -1,5 +1,6 @@
 package com.example.demo.users;
 
+import org.apache.logging.log4j.ThreadContext;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
@@ -9,6 +10,9 @@ public class UserController {
 
     @GetMapping("/user/{id}")
     public UserResponse getUserById(@PathVariable int id){
+        // IP Address
+        String ipAddress = ThreadContext.get("X-IP-ADDRESS");
+        System.out.println("Client's IP Address : " + ipAddress);
         return new UserResponse(1, "Somkiat");
     }
 
