41ca0adaf49a4e77fe3d3f4defa9e5824d790b42
up1 <somkiat.p@gmail.com>
2026-01-08 13:56:57 +0700

    Add more exception

--- demo/src/main/java/com/example/demo/users/UserControllerAdvice.java
+++ demo/src/main/java/com/example/demo/users/UserControllerAdvice.java
@@ -4,13 +4,23 @@ import org.springframework.http.HttpStatusCode;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
+import org.springframework.web.method.annotation.MethodArgumentTypeMismatchException;
 
 @RestControllerAdvice
 public class UserControllerAdvice {
 
     @ExceptionHandler(exception = UserNotFoundException.class)
     public ResponseEntity<ErrorResponse> userNotFound(UserNotFoundException e) {
-        return new ResponseEntity<>(new ErrorResponse(e.getMessage()), HttpStatusCode.valueOf(404));
+        return new ResponseEntity<>(
+                new ErrorResponse(e.getMessage()),
+                HttpStatusCode.valueOf(404));
+    }
+
+    @ExceptionHandler(exception = MethodArgumentTypeMismatchException.class)
+    public ResponseEntity<ErrorResponse> xxxx(Exception e) {
+        return new ResponseEntity<>(
+                new ErrorResponse(e.getMessage()),
+                HttpStatusCode.valueOf(400));
     }
 
 }
