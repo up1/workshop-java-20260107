2530be77a225794a9e47bf2b5ea6599a6af0b50e
up1 <somkiat.p@gmail.com>
2026-01-08 12:10:13 +0700

    Create user controller

---
+++ demo/src/main/java/com/example/demo/users/UserController.java
@@ -0,0 +1,15 @@
+package com.example.demo.users;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class UserController {
+
+    @GetMapping("/user/{id}")
+    public UserResponse getUserById(@PathVariable int id){
+        return new UserResponse(1, "Somkiat");
+    }
+
+}
---
+++ demo/src/main/java/com/example/demo/users/UserResponse.java
@@ -0,0 +1,7 @@
+package com.example.demo.users;
+
+public record UserResponse(
+	int id,
+    String name
+) {
+}
