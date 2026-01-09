66d50b8a49bcf39fcf1129ba6be660718195d4e7
up1 <somkiat.p@gmail.com>
2026-01-09 13:47:55 +0700

    Manage transaction in spring boot

--- demo/src/main/java/com/example/demo/users/UserRepository.java
+++ demo/src/main/java/com/example/demo/users/UserRepository.java
@@ -3,10 +3,15 @@ package com.example.demo.users;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 
+import java.util.List;
 import java.util.Optional;
 
 public interface UserRepository
         extends JpaRepository<MyUser, Integer> {
+
+    // https://docs.spring.io/spring-data/jpa/reference/repositories/query-methods-details.html
+    Optional<List<MyUser>> findByName(String name);
+
 
     // Customize query
     @Query("select id, name from MyUser where id=:userId")
--- demo/src/main/java/com/example/demo/users/UserService.java
+++ demo/src/main/java/com/example/demo/users/UserService.java
@@ -1,5 +1,6 @@
 package com.example.demo.users;
 
+import jakarta.transaction.Transactional;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -11,9 +12,10 @@ public class UserService {
     @Autowired
     private UserRepository userRepository;
 
+    @Transactional
     public UserResponse get(int userId) {
-
-        Optional<MyUser> result = userRepository.getById(userId);
+        userRepository.findById(userId);
+        Optional<MyUser> result = userRepository.findById(userId);
         if(result.isEmpty()){
             // Not found
             throw new UserNotFoundException("Not found");
