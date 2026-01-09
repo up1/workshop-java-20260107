0cbf69a1dd31277d5c1e1d34dc6c403b60a81180
up1 <somkiat.p@gmail.com>
2026-01-09 11:00:29 +0700

    Add prometheus and actuator

--- demo/src/main/java/com/example/demo/DemoApplication.java
+++ demo/src/main/java/com/example/demo/DemoApplication.java
@@ -2,12 +2,16 @@ package com.example.demo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ConfigurableApplicationContext;
 
 @SpringBootApplication
 public class DemoApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(DemoApplication.class, args);
-	}
+		 ConfigurableApplicationContext context
+                 = SpringApplication.run(DemoApplication.class, args);
+         int beanCount = context.getBeanDefinitionCount();
+        System.out.println(beanCount);
+    }
 
 }
