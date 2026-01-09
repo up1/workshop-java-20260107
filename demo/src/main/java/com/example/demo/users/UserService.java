fe244dc6877d2d39fe3a564b3758cd1c0e234c46
up1 <somkiat.p@gmail.com>
2026-01-09 12:08:08 +0700

    Use repository

--- demo/src/main/java/com/example/demo/users/UserService.java
+++ demo/src/main/java/com/example/demo/users/UserService.java
@@ -1,11 +1,25 @@
 package com.example.demo.users;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.util.Optional;
 
 @Service
 public class UserService {
 
+    @Autowired
+    private UserRepository userRepository;
+
     public UserResponse get(int userId) {
+
+        Optional<MyUser> result = userRepository.findById(userId);
+        if(result.isEmpty()){
+            // Not found
+            throw new UserNotFoundException("Not found");
+        }
+        // Found
+
         if(userId == 2) {
             throw new UserNotFoundException("User id=" +  userId + " not found in system");
         }
